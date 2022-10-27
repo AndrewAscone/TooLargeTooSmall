4d84994a686a7ce6cfcb87daeaf1a92661395144
AndrewAscone <andrew.ascone@gmail.com>
2022-10-26 23:48:17 -0400

    Added comments to detail what the program will need and set up the import to use Scanner for user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
+ */
+
+/*
+Things that need to be added to complete game:
+- Random number in a certain range (probably 1-10) using a random number function
+- Input from user to make their guess
+- Conditional based on user input against actual answer
+- A loop that will repeat until the correct number is guessed (probably while or do while)
+- An iterator in the loop to track the number of guesses
+- A conditional to prevent a repeated number from counting as another guess
+
+Possible variables:
+- randomNum (randomly chosen number to guess)
+- userGuess (User's guess for the number)
+- correctGuess (Boolean state for the loop to check if the user has guessed correctly)
+- totalGuess (Number of attempts to guess the correct number)
+- prevGuess (Store the previous guess to check it against the next guess and determine if totalGuess will be incremented)
  */
 public class Main {
 
     public static void main(String[] args){
-        
+
     }
 }
